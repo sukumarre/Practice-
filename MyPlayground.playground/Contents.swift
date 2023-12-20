b926c0f1a049e00bf9d46868185306f22beede64
sukumar <sukkureddy3940@gmail.com>
2023-12-20 11:58:07 -0600

    fibnonic series

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+print(greeting)
+
+
+func fib(_ n:Int)->Int {
+    
+    if n == 0 {
+        return 0
+    }
+    if n == 1 {
+        return 1
+    }
+    
+    return fib(n-1) + fib(n-2)
+    
+}
+
+
+fib(4)
+
+
